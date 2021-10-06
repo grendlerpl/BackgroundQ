2a462a5e82f95df1c7c2fd8c1be8008658ffdf46
Grendler <55236412+grendlerpl@users.noreply.github.com>
2021-10-07 00:42:52 +0200

    Added missing file for service extension

---
+++ BackgroundQServiceExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+
+namespace CodeServiceJD.BackgroundQ
+{
+    public static class BackgroundQServiceExtension
+    {
+        public static IServiceCollection AddQService<QMessage,QProcessor>(this IServiceCollection services)
+            where QProcessor : class, IBackgroundQProcessor<QMessage>
+            {
+            services.AddSingleton<IBackgroundQ<QMessage>, BackgroundQ<QMessage>>();
+            services.AddSingleton<IBackgroundQProcessor<QMessage>, QProcessor>();
+            services.AddHostedService<BackgroundQService<QMessage>>();
+            return services;
+            }
+       
+    }
+
+}
